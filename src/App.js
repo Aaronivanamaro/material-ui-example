e33c3ab14ae870af3da50be6574f62ef2e74708e
Aaronivanamaro <aaronivanamaro@gmail.com>
2021-11-24 20:28:07 -0300

    Routes Correction

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import { Route, Router } from "react-router";
+import { BrowserRouter as Router, Routes, Route } from "react-router";
 import Notes from './pages/Notes'
 import Create from './pages/Create'
 
 function App() {
   return (
     <Router>
-      <Route path="/" element={<Notes />}/>
-      <Route path="/create" element={<Create />}/>
+      <Routes>
+        <Route path="/" element={<Notes />} />
+        <Route path="/create" element={<Create />} />
+      </Routes>
     </Router>    
   );
 }
