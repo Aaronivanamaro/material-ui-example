aef5a4651f9b36b12c418f1be1b05eef9875ff57
Aaronivanamaro <aaronivanamaro@gmail.com>
2021-11-25 01:36:40 -0300

    Video 2 content finished

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,43 +1,94 @@
-import { KeyboardArrowRight, KeyboardArrowRightSharp, Send } from "@mui/icons-material";
-import { Container, Button, ButtonGroup, Typography } from "@mui/material";
+import { Send } from "@mui/icons-material";
+import { Container, Button, Typography, TextField, FormControl, FormLabel, RadioGroup, FormControlLabel, Radio } from "@mui/material";
+import { useState } from "react";
 
 export default function Create() {
 
+    const [title, setTitle] = useState('')
+    const [titleError, setTitleError] = useState(false)
+    const [details, setDetails] = useState('')
+    const [detailsError, setDetailsError] = useState(false)
+    const [category, setCategory] = useState('')
+    const [categoryError, setCategoryError] = useState(false)
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setTitleError(false)
+        setDetailsError(false)
+        setCategoryError(false)
+
+        if (title === '') {
+            setTitleError(true)
+        }
+        if (details === '') {
+            setDetailsError(true)
+        }
+        if (category === '') {
+            setCategoryError(true)
+        }
+        if (title && details && category) {
+            console.log(title, details, category);
+        }
+    }
+
     return (
-        // Container creates a margin
-        // disableGutters removes the padding
-        // maxWidth: 'xs','sm', 'md', 'lg', 'xl', false
-        <Container >
-
-         {/* align: 'center', 'inherit', 'justify', 'left', 'right' */}
-
-            <Typography variant ="h3" gutterBottom component="h1" color="primary"
-                // How to override default styles with sx
-                // sx={{color: 'green', '&:hover': {color: 'green'}}}
-            >
+        <Container maxWidth="sm">
+            <Typography variant ="h3" gutterBottom component="h1" color="primary">
                 Create a New Note
             </Typography>
             
-        {/* color: 'primary', 'secondary', 'success', 'error', 'info', 'warning' */}
-        {/* variant: 'contained', 'outlined', 'text' */}
-            <Button 
-                type="submit" variant="contained" disableElevation 
-                onClick={() => console.log('You clicked me!')}
-                startIcon={<Send />}
-                endIcon={<KeyboardArrowRight />} >
-                Submit
-            </Button>
+            {/* Form components must be inside a simple form element */}
+            {/* noValidate is in order to manage errors with states */}
+            <form noValidate onSubmit={handleSubmit}>
+                {/* variant: 'filled', 'outlined', 'standard' */}
+                {/* same clrs as buttons */}
+                <TextField 
+                    onChange={e => setTitle(e.target.value)}
+                    label="Note Title"
+                    variant="outlined"
+                    color="secondary"
+                    required
+                    fullWidth
+                    margin="normal"
+                    error={titleError}
+                />
+
+                <TextField 
+                    onChange={e => setDetails(e.target.value)}
+                    label="Details"
+                    variant="outlined"
+                    color="secondary"
+                    required
+                    fullWidth
+                    // When multiline is set to true, you can choose
+                    // how many rows to show
+                    multiline
+                    rows={4}
+                    margin="normal"
+                    error={detailsError}
+                />
+
+                <FormControl sx={{display: 'block'}} margin='normal' error={categoryError}>
+                    <FormLabel>Note Category</FormLabel> 
+                    {/* Without wrapping the options in a group, you may select many */}
+                    <RadioGroup value={category} onChange={e => setCategory(e.target.value)}> 
+                        <FormControlLabel value="money" control={<Radio />} label="Money" />
+                        <FormControlLabel value="todos" control={<Radio />} label="Todos" />
+                        <FormControlLabel value="reminders" control={<Radio />} label="Reminders" />
+                        <FormControlLabel value="work" control={<Radio />} label="Work" />
+                    </RadioGroup>                   
+                </FormControl>
+
+                <Button
+                    type="submit" variant="contained"  
+                    endIcon={<Send />} 
+                    // Abreviation de certain CSS props
+                    sx={{mt: 1}}>
+                    Submit
+                </Button>
+
+            </form>
             
-            <ButtonGroup color="secondary" variant="contained">
-                <Button disabled>One</Button>
-                <Button>Two</Button>
-                <Button>Three</Button>
-            </ButtonGroup>
-
-        {/* color: 'primary', 'secondary', 'success', 'action', 'disabled' */}
-        {/* fontSize: 'small', 'large' */}
-            <KeyboardArrowRightSharp fontSize="large" color="success"/>
-
         </Container>
     )
 }
